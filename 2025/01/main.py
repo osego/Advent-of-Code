35e3c345d9954c6c84ee690c2f2ffd3dc9e0c272
osego <60160526+osego@users.noreply.github.com>
2025-12-02 23:08:32 +0100

    feat: day 1 part 2 solution
    It has been a few years since I last did AoC, so I completely forgot about the possibility of part 2 until I solved one for day 2.

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -7,17 +7,28 @@ def parse_input() -> Iterable[int]:
     return map(int, input_str.replace("R", "").replace("L", "-").split())
 
 
-def main() -> int:
-    result = 0
+def hundreds_distance(x: int, y: int) -> int:
+    if y < 0:
+        return (x - 1) // 100 - (x + y - 1) // 100
+    else:
+        return (x + y) // 100 - x // 100
+
+
+def main() -> tuple[int, int]:
+    part_one = 0
+    part_two = 0
+
     counter = 50
 
     operations = parse_input()
     for op in operations:
+        part_two += hundreds_distance(counter, op)
+
         counter += op
         if counter % 100 == 0:
-            result += 1
+            part_one += 1
 
-    return result
+    return part_one, part_two
 
 
 if __name__ == "__main__":
